c36444c54d95924b457a52be6feca2a424316f46
rohit267singh25-cloud <rohit267singh25@gmail.com>
2026-07-12 02:47:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,44 @@
 from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 
-class MyApp(App):
-    def build(self):
-        return Button(text="Hello from Python!")
+class Calculator(GridLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.cols = 4
 
-MyApp().run()
+        self.display = TextInput(multiline=False, readonly=True, font_size=32)
+        self.add_widget(self.display)
+
+        buttons = [
+            "7","8","9","/",
+            "4","5","6","*",
+            "1","2","3","-",
+            "0",".","=","+",
+            "C"
+        ]
+
+        for b in buttons:
+            btn = Button(text=b, font_size=24)
+            btn.bind(on_press=self.on_button)
+            self.add_widget(btn)
+
+    def on_button(self, instance):
+        text = instance.text
+
+        if text == "C":
+            self.display.text = ""
+        elif text == "=":
+            try:
+                self.display.text = str(eval(self.display.text))
+            except:
+                self.display.text = "Error"
+        else:
+            self.display.text += text
+
+class CalculatorApp(App):
+    def build(self):
+        return Calculator()
+
+CalculatorApp().run()
